94187033f95f46228922dc3e5adf02b05af479ad
Souless911 <monteroespina@hotmail.com>
2021-05-12 16:14:05 -0500

    updated stuff
    login que no existe ya esta linkeado al backend

---
+++ Servidor/Front-End/back-end/front.js
@@ -0,0 +1,63 @@
+"use strict"
+
+const HTTTPMethods = {
+    "put": "PUT",
+    "post": "POST",
+    "get": "GET",
+    "delete": "DELETE"
+}
+
+const urlpage = ''
+let modalLogin = document.getElementById("loginid");
+let btnLogin = document.getElementById("btnLogin");
+modalLogin.addEventListener("change", function (e) {
+    let invalids = document.querySelectorAll("#loginId input:invalid");
+    let valids = document.querySelectorAll("#loginId input:valid");
+    for (let e of invalids) e.classList.add("border", "border-danger");
+    for (let e of valids) e.classList.remove("border", "border-danger");
+    if (invalids.length == 0) btnLogin.removeAttribute('disabled');
+    else btnLogin.setAttribute('disabled', null);
+});
+
+btnLogin.onclick = function (event) {
+    let d = {};
+    d.email = document.getElementById("correo").value;
+    d.password = document.getElementById("password").value;
+    log(JSON.stringify(d));
+    event.preventDefault();
+}
+
+function log(f) {
+    sendHTTPRequest('/api/login', f, HTTTPMethods.post, (res) => {
+        console.log(res);
+    }, (err, res) => {
+        console.log(err, res);
+    });
+}
+
+function sendHTTPRequest(urlAPI, data, method, cbOK, cbError) {
+    // 1. Crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. Configurar:  PUT actualizar archivo
+    xhr.open(method, urlAPI);
+    // 3. indicar tipo de datos JSON
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    //console.log(TOKEN);
+    //xhr.setRequestHeader('x-auth-user', TOKEN);
+    // 4. Enviar solicitud al servidor
+    xhr.send(data);
+    // 5. Una vez recibida la respuesta del servidor
+    xhr.onload = function () {
+        if (xhr.status != 200) { // analizar el estatus de la respuesta HTTP 
+            // Ocurrió un error
+            alert(xhr.status + ': ' + xhr.responseText); // e.g. 404: Not Found
+            cbError(xhr.status + ': ' + xhr.responseText);
+        } else {
+            // console.log(xhr.responseText); // Significa que fue exitoso
+            cbOK({
+                status: xhr.status,
+                data: xhr.responseText
+            });
+        }
+    };
+}
